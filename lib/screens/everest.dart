447d2620d2345340d998c677a3bfdfbe3780fbc2
Patricio Ramos Castro <patricio.ramos.cl@gmail.com>
2024-12-02 11:28:31 -0300

    Se cambia versión de la aplicación y se prueba la configuración para dejar el APK con un nombre distinto al por defecto.

--- lib/screens/everest.dart
+++ lib/screens/everest.dart
@@ -56,7 +56,7 @@ class _EverestState extends State<Everest> {
               }
             })),
       body: const Center(
-        child: Text('Versión Beta 1.1.1'),
+        child: Text('Versión Beta 1.2.0'),
       ),
     );
   }
