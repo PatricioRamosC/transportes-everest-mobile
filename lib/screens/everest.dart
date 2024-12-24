5a79e094514487a6e6e54b00264c97d9aed3303a
Patricio Ramos Castro <extpramosc@transbank.cl>
2024-12-24 08:35:27 -0300

    El envío de SMS tiene un comportamiento errático donde aparece como si el destinatario le hubiese enviado el SMS al remitente.

--- lib/screens/everest.dart
+++ lib/screens/everest.dart
@@ -56,7 +56,7 @@ class _EverestState extends State<Everest> {
               }
             })),
       body: const Center(
-        child: Text('Versión Beta 1.4.2'),
+        child: Text('Versión Beta 1.4.3'),
       ),
     );
   }
